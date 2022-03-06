e00b3fa894ffa902dd123f999a200f5a25fc95f7
kiosion <hi@kio.dev>
2022-03-06 17:49:55 -0400

    Change dirs, create example .htaccess

--- includes/components/footer.php
+++ includes/components/footer.php
@@ -1,5 +1,6 @@
 <?php
 
+// Prevent direct access
 if (!isset($include)) {
 	header("Location: ../../");
 }
--- includes/styles.php
+++ includes/styles.php
@@ -1,3 +1,3 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@4cac1a6/css/all.css" rel="stylesheet" type="text/css" />
-<link href="/css/styles.css" rel="stylesheet">
+<link href="/resources/css/styles.css" rel="stylesheet">
